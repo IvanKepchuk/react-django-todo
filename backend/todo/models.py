a0d4d4a051a5fbd62e6cc2c9495cf03f8ddd4700
IvanKepchuk <graffkepinberg@gmail.com>
2023-05-11 18:00:26 +0900

    app todo models

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
-    completed = models.BooleanField=False
+    completed = models.BooleanField(default=False)
     #completed - task status
 
     def _str_(self):
