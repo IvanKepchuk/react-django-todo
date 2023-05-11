d1318156d0affefa25edf9fbfdfce50fce526960
IvanKepchuk <graffkepinberg@gmail.com>
2023-05-11 17:42:30 +0900

    app todo models

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#model properties todo
+class Todo(models.Models):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField=False
+    #completed - task status
+
+    def _str_(self):
+        return self.title
+    
